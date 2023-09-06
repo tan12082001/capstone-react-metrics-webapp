ffdea07b2ec8129a5a9ba82dac84f30a78fd555e
Tanmayi Manku <tanmayitanum@gmail.com>
2023-09-06 23:49:47 +0530

    implement routes for home, details

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import '../styles/App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
+import { fetchEarthquakesData } from '../redux/items/itemsSlice';
+import Home from './Homepage';
+import Details from './DetailsPage';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchEarthquakesData());
+  }, [dispatch]);
   return (
-    <h1>Testing</h1>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/items/:id" element={<Details />} />
+      </Routes>
+    </Router>
   );
 }
 
