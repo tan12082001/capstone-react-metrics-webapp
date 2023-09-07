cfd411f2a11cf1783e36168aaaf270e3f6e0e230
Tanmayi Manku <tanmayitanum@gmail.com>
2023-09-07 20:50:06 +0530

    add setmagnitude reeducer and update api link

--- src/redux/items/itemsSlice.js
+++ src/redux/items/itemsSlice.js
@@ -1,13 +1,22 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const initialState = {
+  items: [],
+  isLoading: false,
+  error: undefined,
+  filter: {
+    minmagnitude: 8,
+  },
+};
+
 export const fetchEarthquakesData = createAsyncThunk(
   'items/fetchEarthquakesData',
   async (_, thunkAPI) => {
+    const minmagnitude = thunkAPI.getState().items.filter;
     try {
-      const response = await axios.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2001-01-01&endtime=2023-01-01&minmagnitude=8');
+      const response = await axios.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2001-01-01&endtime=2023-01-01&minmagnitude=${minmagnitude.minmagnitude}`);
       const { data } = response;
-      console.log(data.features);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -15,16 +24,14 @@ export const fetchEarthquakesData = createAsyncThunk(
   },
 );
 
-const initialState = {
-  items: [],
-  isLoading: false,
-  error: undefined,
-};
-
 const itemsSlice = createSlice({
   name: 'items',
   initialState,
-  reducers: {},
+  reducers: {
+    setMinMagnitude: (state, action) => {
+      state.filter.minmagnitude = action.payload;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchEarthquakesData.pending, (state) => {
@@ -41,4 +48,5 @@ const itemsSlice = createSlice({
   },
 });
 
+export const { setMinMagnitude } = itemsSlice.actions;
 export default itemsSlice.reducer;
